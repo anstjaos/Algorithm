d99c5dc49626382c82ae731a530a3bc4ddf5f3c9
moonseo.kim <moonseo.kim@nhn.com>
2021-07-08 13:27:21 +0900

    210708 BOJ

---
+++ BOJ/Java/BOJ_5337.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class BOJ_5337 {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append(".  .   .\n")
+                .append("|  | _ | _. _ ._ _  _\n")
+                .append("|/\\|(/.|(_.(_)[ | )(/.\n");
+
+        System.out.print(sb);
+    }
+}
---
+++ BOJ/Java/BOJ_5338.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class BOJ_5338 {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("       _.-;;-._\n")
+                .append("'-..-'|   ||   |\n")
+                .append("'-..-'|_.-;;-._|\n")
+                .append("'-..-'|   ||   |\n")
+                .append("'-..-'|_.-''-._|");
+
+        System.out.println(sb);
+    }
+}
