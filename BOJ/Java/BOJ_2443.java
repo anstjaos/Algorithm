0816b8085bf6f721f7f438793ed72faa6c7866b5
moonseo.kim <moonseo.kim@nhn.com>
2021-08-26 11:39:22 +0900

    210826 BOJ

---
+++ BOJ/Java/BOJ_2443.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class BOJ_2443 {
+    public static void main(String[] args) throws IOException {
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) sb.append(" ");
+            for (int j = 0; j < N - i; j++) sb.append("*");
+            for (int j = 0; j < N - i - 1; j++) sb.append("*");
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
