c99ccebea7d2f9529104369dc69af28b54f8fedc
moonseo.kim <moonseo.kim@nhn.com>
2021-11-06 20:19:19 +0900

    211106 BOJ

---
+++ BOJ/Java/BOJ_17298.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class BOJ_17298 {
+    public static void main(String[] args) throws IOException {
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+        br.close();
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < N; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                arr[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            arr[stack.pop()] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) sb.append(arr[i]).append(" ");
+
+        System.out.println(sb);
+    }
+}
