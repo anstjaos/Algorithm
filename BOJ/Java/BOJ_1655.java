5484011289b244fc476eb887a30697e3dee320f5
moonseo.kim <moonseo.kim@nhn.com>
2021-11-16 16:32:54 +0900

    211116 BOJ

---
+++ BOJ/Java/BOJ_1655.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.PriorityQueue;
+
+public class BOJ_1655 {
+    public static void main(String[] args) throws IOException {
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((o1, o2) -> o1 - o2);
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((o1, o2) -> o2 - o1);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (minHeap.size() == maxHeap.size()) maxHeap.offer(num);
+            else minHeap.offer(num);
+
+            if(!minHeap.isEmpty() && !maxHeap.isEmpty())
+                if(minHeap.peek() < maxHeap.peek()){
+                    int tmp = minHeap.poll();
+                    minHeap.offer(maxHeap.poll());
+                    maxHeap.offer(tmp);
+                }
+
+            sb.append(maxHeap.peek() + "\n");
+        }
+        br.close();
+
+        System.out.println(sb);
+    }
+}
