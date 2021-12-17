748f81af1b139b57d12675b323406f5bebb711d9
moonseo.kim <moonseo.kim@nhn.com>
2021-12-17 15:06:44 +0900

    211217 BOJ

---
+++ BOJ/Java/BOJ_15802.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class BOJ_15802 {
+    public static void main(String[] args) throws IOException {
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(1);
+    }
+}
