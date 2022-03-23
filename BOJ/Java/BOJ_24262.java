2e36dd68e4f0769912c31c255d79d73b5f25d222
moonseo.kim <moonseo.kim@nhn.com>
2022-03-23 18:18:13 +0900

    220323 BOJ

---
+++ BOJ/Java/BOJ_24262.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class BOJ_24262 {
+    public static void main(String[] args) throws IOException {
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+
+//        br.close();
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
