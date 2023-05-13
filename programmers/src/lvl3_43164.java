708dc9668f2e8049dede69551bb420385615169c
moonseo.kim <moonseo.kim@nhn.com>
2023-05-13 20:35:12 +0900

    230513 leetcode

---
+++ programmers/src/lvl3_43164.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class lvl3_43164 {
+    boolean[] visited;
+    List<String> answers;
+
+    public String[] solution(String[][] tickets) {
+        visited = new boolean[tickets.length];
+        answers = new ArrayList<String>();
+        int count = 0;
+
+        dfs(count, "ICN", "ICN", tickets);
+        Collections.sort(answers);
+        String[] answer = answers.get(0).split(" ");
+        return answer;
+    }
+
+    public void dfs(int count, String present, String answer, String[][]ticktes) {
+        if(count == ticktes.length) {
+            answers.add(answer);
+            return;
+        }
+
+        for(int i = 0; i < ticktes.length; i++) {
+            if(!visited[i] && ticktes[i][0].equals(present)) {
+                visited[i] = true;
+                dfs(count+1, ticktes[i][1], answer + " " + ticktes[i][1], ticktes);
+                visited[i] = false;
+            }
+        }
+        return;
+    }
+}
