374cb02a9be56116f915dd93b0937f20caf41fda
anstjaos <moonseo.kim@nhn.com>
2023-05-20 18:32:52 +0900

    230520 programmers

---
+++ programmers/src/lvl3_43105.java
@@ -0,0 +1,19 @@
+public class lvl3_43105 {
+    public int solution(int[][] triangle) {
+        int[][] dp = new int[triangle.length][502];
+        dp[0][0] = triangle[0][0];
+
+        for (int i = 0; i < triangle.length - 1; i++) {
+            for (int j = 0; j < triangle[i].length; j++) {
+                dp[i + 1][j] = Math.max(dp[i + 1][j], dp[i][j] + triangle[i + 1][j]);
+                dp[i + 1][j + 1] = Math.max(dp[i + 1][j + 1], dp[i][j] + triangle[i + 1][j + 1]);
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < triangle[triangle.length - 1].length; i++) {
+            answer = Math.max(answer, dp[triangle.length - 1][i]);
+        }
+        return answer;
+    }
+}
